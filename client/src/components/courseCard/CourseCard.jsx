0e9b564f89b852e2dc38c60bb291b7b5bfc22258
Felipe Aguayo <felipeaguayo212@icloud.com>
2023-09-04 14:45:20 -0600

    fix for images

--- client/src/components/courseCard/CourseCard.jsx
+++ client/src/components/courseCard/CourseCard.jsx
@@ -1,17 +1,61 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import img1 from '../../assets/images/advanced-algorithms.png';
+import img2 from '../../assets/images/Artificial-Intelligence.png';
+import img3 from '../../assets/images/Compiler-Design.png';
+import img4 from '../../assets/images/computer-architecture.png';
+import img5 from '../../assets/images/Computer-Graphics.png';
+import img6 from '../../assets/images/Computer-Networking.png';
+import img7 from '../../assets/images/computer-vision.png';
+import img8 from '../../assets/images/data-structures.png';
+import img9 from '../../assets/images/Database-Design-&-Management.png';
+import img10 from '../../assets/images/Database-Design-and-Management.png';
+import img11 from '../../assets/images/intro-to-computer-science.png';
+import img12 from '../../assets/images/Introduction-to-Information-Systems.png';
+import img13 from '../../assets/images/Machine-Learning.png';
+import img14 from '../../assets/images/networking-and-security.png';
+import img15 from '../../assets/images/Object-Oriented-Programming.png';
+import img16 from '../../assets/images/Operating-Systems.png';
+import img17 from '../../assets/images/Parallel-Computing.png';
+import img18 from '../../assets/images/Software-Engineering.png';
+import img19 from '../../assets/images/Software-Testing-&-Verification.png';
+import img20 from '../../assets/images/Systems-Analysis-and-Design.png';
+import img21 from '../../assets/images/Systems-Programming.png';
+import img22 from '../../assets/images/Web-Design-and-Development.png';
 
 const CourseCard = ({ course }) => {
   const { user } = useSelector((store) => store.user);
-  console.log(img1);
+  const images = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20,
+    img21,
+    img22,
+  ];
+
+  const imgSrc = images.filter((image) => image === course.img_address);
+
   return (
     <article className="card">
       <div className="img-card">
-        <img
-          src={course.img_address === img1 ? img1 : null}
-          alt={course.course_title}
-        />
+        <img src={imgSrc} alt={course.course_title} />
       </div>
       <div className="course-name">
         <Link
